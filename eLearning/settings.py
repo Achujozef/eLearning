fe9b60381f856a0814e12127004c3e65f06c95fd
achujoseph0709 <achujozefsl0709@gmail.com>
2023-12-05 17:25:02 +0530

    hosting

--- eLearning/settings.py
+++ eLearning/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-_!)msvf3j+!qdzkq53qamib3(du05_zl4cnx565qikqob869h(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
